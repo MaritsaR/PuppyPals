7d2f0b125528ae025e51ee8d764152ae27975872
Maritsa <maritsar28@gmail.com>
2023-11-04 22:31:15 -0500

    CSS changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,30 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => (
-        <p key={puppy.id}>{puppy.name}</p>
+        <p
+          onClick={() => {
+            setFeatPupId(puppy.id);
+          }}
+          key={puppy.id}
+        >
+          {puppy.name}
+        </p>
       ))}
     </div>
   );
